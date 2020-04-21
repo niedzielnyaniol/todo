a0e4429469e0aab75e898d15b7f4c6cf65278409
Maciej Kaczmarczyk <maciej_kaczmarczyk@tvn.pl>
2020-04-21 20:51:05 +0200

    style(eslint): rm errors

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,47 +1,47 @@
-import * as express from "express";
-import {Request, Response} from "express";
-import * as bodyParser from  "body-parser";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import { createConnection } from 'typeorm';
+import { User } from './entity/User';
 
 // create typeorm connection
-createConnection().then(connection => {
-    const userRepository = connection.getRepository(User);
+createConnection().then((connection) => {
+  const userRepository = connection.getRepository(User);
 
-    // create and setup express app
-    const app = express();
-    app.use(bodyParser.json());
+  // create and setup express app
+  const app = express();
+  app.use(bodyParser.json());
 
-    // register routes
+  // register routes
 
-    app.get("/users", async function(req: Request, res: Response) {
-        const users = await userRepository.find();
-        res.json(users);
-    });
+  app.get('/users', async (req: Request, res: Response) => {
+    const users = await userRepository.find();
+    res.json(users);
+  });
 
-    app.get("/users/:id", async function(req: Request, res: Response) {
-        const results = await userRepository.findOne(req.params.id);
-        return res.send(results);
-    });
+  app.get('/users/:id', async (req: Request, res: Response) => {
+    const results = await userRepository.findOne(req.params.id);
+    return res.send(results);
+  });
 
-    app.post("/users", async function(req: Request, res: Response) {
-        const user = await userRepository.create(req.body);
-        const results = await userRepository.save(user);
-        return res.send(results);
-    });
+  app.post('/users', async (req: Request, res: Response) => {
+    const user = await userRepository.create(req.body);
+    const results = await userRepository.save(user);
+    return res.send(results);
+  });
 
-    app.put("/users/:id", async function(req: Request, res: Response) {
-        const user = await userRepository.findOne(req.params.id);
-        userRepository.merge(user, req.body);
-        const results = await userRepository.save(user);
-        return res.send(results);
-    });
+  app.put('/users/:id', async (req: Request, res: Response) => {
+    const user = await userRepository.findOne(req.params.id);
+    userRepository.merge(user, req.body);
+    const results = await userRepository.save(user);
+    return res.send(results);
+  });
 
-    app.delete("/users/:id", async function(req: Request, res: Response) {
-        const results = await userRepository.delete(req.params.id);
-        return res.send(results);
-    });
+  app.delete('/users/:id', async (req: Request, res: Response) => {
+    const results = await userRepository.delete(req.params.id);
+    return res.send(results);
+  });
 
-    // start express server
-    app.listen(3000);
+  // start express server
+  app.listen(3000);
 });
