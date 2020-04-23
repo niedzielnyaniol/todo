055567517712a7b6b70b04d54abb80e4baae2192
Maciej Kaczmarczyk <maciej_kaczmarczyk@tvn.pl>
2020-04-24 00:02:54 +0200

    feat: add monserrat font

--- packages/react/src/components/App.tsx
+++ packages/react/src/components/App.tsx
@@ -70,7 +70,7 @@ const App = (): JSX.Element => {
             <Breadcrumb.Item>Bill</Breadcrumb.Item>
           </Breadcrumb>
           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            Bill is a cat.
+            maciej is a cat.
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
